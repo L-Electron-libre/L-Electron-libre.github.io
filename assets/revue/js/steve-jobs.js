3b4a140c13fc703c020ff380f68ca6b67e156b6c
Érika <59779111+erikaloranger@users.noreply.github.com>
2021-06-02 13:59:58 -0400

    Ajout de deux textes : Le mutisme d'une société et Mémoire GNL. Modification de revue2020.html (qui ne fonctionne toujours pas)

--- assets/revue/js/steve-jobs.js
+++ assets/revue/js/steve-jobs.js
@@ -30,7 +30,7 @@ function updateDepth(book, newPage) {
 
 function loadPage(page) {
 
-	$.ajax({url: 'pages/page' + page + '.html'}).
+	$.ajax({url: 'revues/revue2020/pages/page' + page + '.html'}).
 		done(function(pageHtml) {
 			$('.sj-book .p' + page).html(pageHtml.replace('samples/steve-jobs/', ''));
 		});
@@ -98,7 +98,7 @@ function zoomThis(pic) {
 				tmpContainer.appendTo($('body'));
 
 				$('body').css({'overflow': 'hidden'});
-				
+
 				tmpPic.css({
 					margin: position.top + 'px ' + position.left+'px'
 				}).
@@ -113,7 +113,7 @@ function zoomThis(pic) {
 		$('.sj-book').turn('disable', true);
 
 		$(window).resize(zoomOut);
-		
+
 		tmpContainer.click(zoomOut);
 
 		tmpPic.load(function() {
@@ -138,8 +138,8 @@ function zoomThis(pic) {
 
 			$('.samples .bar').css({visibility: 'hidden'});
 			$('#slider-bar').hide();
-			
-		
+
+
 			$('#book-zoom').transform(
 				'translate('+translate.left+'px, '+translate.top+'px)' +
 				'scale('+zoomFactor+', '+zoomFactor+')');
@@ -193,7 +193,7 @@ function setPreview(view) {
 
 	var previewWidth = 115,
 		previewHeight = 73,
-		previewSrc = 'pages/preview.jpg',
+		previewSrc = 'revues/revue2020/pages/preview.jpg',
 		preview = $(_thumbPreview.children(':first')),
 		numPages = (view==1 || view==$('#slider').slider('option', 'max')) ? 1 : 2,
 		width = (numPages==1) ? previewWidth/2 : previewWidth;
